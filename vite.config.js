949b1822e9276b99c19b075436f6e1202d814410
ayyagarysudhira <ayyagarysudhira@yahoo.com>
2026-01-26 12:23:39 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
- https:'/ayyagarysudhira.github.io/My-Resume/'
+  base: "https:/ayyagarysudhira.github.io/My-Resume/"
 })
